09ad8dcd41b5587241a4c93b6615a3bb7d2af0f0
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-20 16:44:50 -0500

    migrate to bun

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { tw } from "@/utils/tailwind";
 
 const Index = () => {
   return (
-    <View style={tw.style("flex-1 items-center justify-center")}>
+    <View style={tw.style("items-center justify-center flex-1")}>
       <Text
         style={tw.style("text-sky-300 font-semibold tracking-widest text-xl")}
       >
