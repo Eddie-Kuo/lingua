1f09ac1fbf275a810c8397b31d22b7fc8ceb98bb
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-20 16:38:41 -0500

    install and add twrnc

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { tw } from "@/utils/tailwind";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={tw.style("flex-1 items-center justify-center")}>
+      <Text
+        style={tw.style("text-sky-300 font-semibold tracking-widest text-xl")}
+      >
+        Hello World
+      </Text>
     </View>
   );
 };
