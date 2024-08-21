7b60f1668ad4f298b816a4591a5e486ca6d17092
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-21 16:17:54 -0500

    add cta button to sign in

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Button, Pressable } from "react-native";
 import React from "react";
 import { tw } from "@/utils/tailwind";
+import { Link } from "expo-router";
 
 const Index = () => {
   return (
-    <View style={tw.style("items-center justify-center flex-1 bg-secondary")}>
+    <View
+      style={tw.style(
+        "items-center justify-center flex-1 bg-secondary relative"
+      )}
+    >
       <View style={tw.style("flex items-center justify-center gap-2")}>
         <Image
           source={require("@/assets/images/icon.png")}
@@ -25,6 +30,19 @@ const Index = () => {
         >
           Speak Freely, Text Clearly
         </Text>
+      </View>
+      <View style={tw.style("w-[100%] items-center absolute bottom-10")}>
+        <Link asChild href={"/(public)"}>
+          <Pressable
+            style={tw.style(
+              "w-[80%] bg-highlightAccent items-center py-3 rounded-full"
+            )}
+          >
+            <Text style={tw.style("text-white font-semibold text-lg")}>
+              Start Messaging
+            </Text>
+          </Pressable>
+        </Link>
       </View>
     </View>
   );
