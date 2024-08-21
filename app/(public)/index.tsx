8c632a6714c7d0667f4a8252b00f857e5061f9f5
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-21 12:52:05 -0500

    add in logo

--- app/(public)/index.tsx
+++ app/(public)/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { tw } from "@/utils/tailwind";
 
 const Login = () => {
   return (
-    <View>
+    <View style={tw.style("items-center justify-center flex-1 bg-undertone")}>
       <Text>Login</Text>
     </View>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { tw } from "@/utils/tailwind";
 
 const Index = () => {
   return (
-    <View style={tw.style("items-center justify-center flex-1 bg-undertone")}>
-      <Text
-        style={tw.style("text-secondary font-bold tracking-widest text-xl")}
-      >
-        Hello World
-      </Text>
+    <View style={tw.style("items-center justify-center flex-1 bg-secondary")}>
+      <View style={tw.style("flex items-center justify-center gap-2")}>
+        <Image
+          source={require("@/assets/images/icon.png")}
+          resizeMode="contain"
+          style={tw.style("w-64 h-52")}
+        />
+      </View>
     </View>
   );
 };
