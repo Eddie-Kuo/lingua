bf82375cbeb0032515d8178d57294e3db95b7232
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-23 22:44:28 -0500

    add prettier plugin for class sorting

--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,41 @@
 import { View, Text, Image, Button, Pressable } from "react-native";
 import React from "react";
-import { tw } from "@/utils/tailwind";
+// import { tw } from "@/utils/tailwind";
 import { Link } from "expo-router";
+import tw from "twrnc";
 
 const Index = () => {
   return (
     <View
       style={tw.style(
-        "items-center justify-center flex-1 bg-secondary relative"
-      )}
-    >
+        "relative flex-1 items-center justify-center bg-secondary",
+      )}>
       <View style={tw.style("flex items-center justify-center gap-2")}>
         <Image
           source={require("@/assets/images/icon.png")}
           resizeMode="contain"
-          style={tw.style("w-64 h-52")}
+          style={tw.style("h-52 w-64")}
         />
         <Text
           style={tw.style(
-            "text-highlight font-semibold tracking-widest text-3xl"
-          )}
-        >
+            "text-3xl font-semibold tracking-widest text-highlight",
+          )}>
           Lingua
         </Text>
         <Text
           style={tw.style(
-            "text-highlightAccent font-medium tracking-widest text-xl"
-          )}
-        >
+            "text-xl font-medium tracking-widest text-highlightAccent",
+          )}>
           Speak Freely, Text Clearly
         </Text>
       </View>
-      <View style={tw.style("w-[100%] items-center absolute bottom-10")}>
+      <View style={tw.style("absolute bottom-10 w-[100%] items-center")}>
         <Link asChild href={"/(public)"}>
           <Pressable
             style={tw.style(
-              "w-[80%] bg-highlightAccent items-center py-3 rounded-full"
-            )}
-          >
-            <Text style={tw.style("text-white font-semibold text-lg")}>
+              "w-[80%] items-center rounded-full bg-highlightAccent py-3",
+            )}>
+            <Text style={tw.style("text-lg font-semibold text-white")}>
               Start Messaging
             </Text>
           </Pressable>
