284259894fcaa02c3e2f5ba3a2ffc5641a1a41e9
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-25 16:50:33 -0500

    install secure store for token cache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Slot } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const InitialLayout = () => {};
 
 const RootLayout = () => {
   return <Slot />;
