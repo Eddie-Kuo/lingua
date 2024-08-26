831e6704d0bdea06646d3cc05cdc0044202c5b5c
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-25 21:53:08 -0500

    change async storage to expo secure store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,4 @@
 import { Slot } from "expo-router";
-import * as SecureStore from "expo-secure-store";
-
-const tokenCache = {
-  async getToken(key: string) {
-    try {
-      return SecureStore.getItemAsync(key);
-    } catch (err) {
-      return null;
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch (err) {
-      return;
-    }
-  },
-};
 
 const InitialLayout = () => {
   return <Slot />;
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,13 +1,26 @@
 import "react-native-url-polyfill/auto";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
+
+// Custom secure storage solution for the Supabase client to store the JWT
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
   auth: {
-    storage: AsyncStorage,
+    storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
