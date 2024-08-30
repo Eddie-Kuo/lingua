38c5b64f0035de2dbe9412a31644390042e482f4
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-29 21:15:40 -0500

    onboarding form for user information

---
+++ app/(onboarding)/_layout.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+const OnboardingLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        title: "",
+        headerBackTitle: "",
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: "#274560",
+        },
+      }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
+
+export default OnboardingLayout;
