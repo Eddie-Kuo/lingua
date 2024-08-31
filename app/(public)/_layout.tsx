dcc0a2e947b39be3854cd8e10d9cbc1cae3c6740
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-08-31 00:01:21 -0500

    init language selection screen

--- app/(public)/_layout.tsx
+++ app/(public)/_layout.tsx
@@ -4,18 +4,17 @@ import { Stack, useRouter } from "expo-router";
 const PublicLayout = () => {
   const router = useRouter();
   return (
-    <Stack>
-      <Stack.Screen
-        name="signup"
-        options={{
-          title: "",
-          headerBackTitle: "",
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: "#274560",
-          },
-        }}
-      />
+    <Stack
+      screenOptions={{
+        title: "",
+        headerBackTitle: "",
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: "#274560",
+        },
+      }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="signup" />
     </Stack>
   );
 };
