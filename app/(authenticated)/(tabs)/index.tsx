0420b130613eea61e2d069e803e6ac7827793a7f
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-09-16 14:50:45 -0500

    add notes for next feature

--- app/(authenticated)/(tabs)/index.tsx
+++ app/(authenticated)/(tabs)/index.tsx
@@ -1,31 +1,18 @@
-import { View, Text, Button } from "react-native";
-import React, { useEffect, useState } from "react";
-import { supabase } from "@/utils/supabase";
-import { Session } from "@supabase/supabase-js";
+import { View, Text } from "react-native";
 import { tw } from "@/utils/tailwind";
-import { useAuth } from "@/context/AuthProvider";
 
 const HomeScreen = () => {
-  const [session, setSession] = useState<Session | null>(null);
-  const { signOut } = useAuth();
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
   return (
     <View style={tw.style("flex-1 items-center justify-center bg-primary")}>
-      {session && session.user && <Text>{session.user.id}</Text>}
-      <Button title="sign out" onPress={signOut}></Button>
       <Text>Hello World</Text>
     </View>
   );
 };
 
 export default HomeScreen;
+
+//* Features this screen will be in charge of:
+//Todo: addFriend button - search and add a friend through phone number - bottom modal
+//Todo: Somewhere to show list of pending requests? - bottom modal
+//Todo: list of all friends from friends list - on screen
+//Todo: Notifications area to display list of incoming requests within the app - bottom modal
--- app/(authenticated)/(tabs)/settings.tsx
+++ app/(authenticated)/(tabs)/settings.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { useAuth } from "@/context/AuthProvider";
 
 const SettingScreen = () => {
+  const { signOut } = useAuth();
   return (
     <View>
-      <Text>SettingScreen</Text>
+      <Button title="sign out" onPress={signOut}></Button>
     </View>
   );
 };
