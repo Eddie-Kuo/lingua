d5d837c83e8a3f2f012cb32a6326845a3fc7e32f
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-09-16 14:09:51 -0500

    add header styles

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,10 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Colors } from "@/constants/colors";
 
 const TabsLayout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShadowVisible: false,
+        headerTitle: "Lingua",
+        headerTitleAlign: "left", // may not with with iOS according to docs
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontSize: 32,
+          color: Colors.highlightAccent,
+        },
+        headerStyle: {
+          height: 120,
+          backgroundColor: Colors.secondary,
+        },
+      }}>
       <Tabs.Screen name="index" />
       <Tabs.Screen name="chats" />
       <Tabs.Screen name="settings" />
---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+export const Colors = {
+  white: "#F5F5F5",
+  primary: "#09111B",
+  secondary: "#274560",
+  accent: "#51C3CB",
+  undertone: "#E6EFF5",
+  highlight: "#F3C82C",
+  highlightAccent: "#E4871D",
+};
