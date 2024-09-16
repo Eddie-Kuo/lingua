374527292d2fbdd94ddd65128d3486da0e8bc8c0
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-09-16 14:19:42 -0500

    add icon styles with active icons

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -2,12 +2,15 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { Colors } from "@/constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import { tw } from "@/utils/tailwind";
 
 const TabsLayout = () => {
   return (
     <Tabs
       screenOptions={{
         headerShadowVisible: false,
+        tabBarLabel: "",
         headerTitle: "Lingua",
         headerTitleAlign: "left", // may not with with iOS according to docs
         headerTitleStyle: {
@@ -19,12 +22,55 @@ const TabsLayout = () => {
           height: 120,
           backgroundColor: Colors.secondary,
         },
+        tabBarStyle: {
+          backgroundColor: Colors.secondary,
+          borderTopColor: Colors.undertone,
+          borderTopWidth: 2,
+          height: 90,
+        },
       }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="chats" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon color={color} focused={focused} icon={"home"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chats"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              color={color}
+              focused={focused}
+              icon={"chatbubble-ellipses"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon color={color} focused={focused} icon={"settings"} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
 
 export default TabsLayout;
+
+const TabIcon = ({ focused, icon }: any) => {
+  return (
+    <View style={tw.style("items-center gap-1")}>
+      <Ionicons
+        color={Colors.highlightAccent}
+        size={28}
+        name={focused ? icon : `${icon}-outline`}
+      />
+    </View>
+  );
+};
