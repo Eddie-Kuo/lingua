fe87f1963495f5c2791c381c807f68dd2387dff5
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-10-28 16:35:32 -0500

    cleanup

--- database/queries/messages.ts
+++ database/queries/messages.ts
@@ -19,10 +19,6 @@ export const createMessage = async (message: InsertMessage) => {
   }
 };
 
-// Todo: Function to get messages by conversation ID
-// The function should take in a conversation Id and return a list of messages with the following room ID
-// The messages should be ordered by timestamp in descending order
-
 export const getMessagesByConversationId = async (conversationId: string) => {
   const { data, error } = await supabase
     .from("messages")
