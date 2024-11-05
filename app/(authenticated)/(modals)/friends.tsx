0add353ff6e1d33c19810b397f667ee7ba17f3c2
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-11-05 16:51:10 -0600

    refactor

--- app/(authenticated)/(modals)/friends.tsx
+++ app/(authenticated)/(modals)/friends.tsx
@@ -11,15 +11,36 @@ import React, { useState } from "react";
 import { tw } from "@/utils/tailwind";
 import { Ionicons } from "@expo/vector-icons";
 import { getUserByPhoneNumber, isFriend } from "@/database/queries/user";
-import { UserInfo } from "@/types/user";
+import { Language, UserInfo } from "@/types/user";
 import useUserStore from "@/store/userStore";
+
+const nonExitingUser = {
+  id: 0,
+  phone_number: "",
+  first_name: "",
+  last_name: "",
+  pic_url: "",
+  selected_language: "English",
+};
 
 const Modal = () => {
   const { userInfo } = useUserStore();
   const [number, setNumber] = useState("");
   const [areaCode] = useState("+1");
   const [searchedUser, setSearchedUser] = useState<UserInfo>();
-  const [customMessage, setCustomMessage] = useState("");
+  const [userMessage, setUserMessage] = useState({
+    message: "",
+    buttonText: "",
+  });
+
+  const nonExitingUser: UserInfo = {
+    id: 0,
+    phone_number: "",
+    first_name: "",
+    last_name: "",
+    pic_url: "",
+    selected_language: Language.English,
+  };
 
   const handleSearchForFriend = async () => {
     const fullPhoneNumber = `${areaCode}${number}`;
@@ -30,19 +51,28 @@ const Modal = () => {
     }
 
     getUserByPhoneNumber(fullPhoneNumber).then(async (user) => {
+      console.log("🚀 ~ getUserByPhoneNumber ~ user:", user);
       if (!user) {
-        setCustomMessage(
-          "No user by that phone number found. Please check the phone number you entered and try again!",
-        );
+        setUserMessage({
+          message: "No user by that phone number found.",
+          buttonText: "",
+        });
+        setSearchedUser(nonExitingUser);
         return;
       }
 
       // check if searchedUser is already a friend
       const isAlreadyFriends = await isFriend(userInfo.id, user.id);
       if (isAlreadyFriends) {
-        setCustomMessage("is already your friend!");
+        setUserMessage({
+          message: "is already your friend!",
+          buttonText: "Chat",
+        });
       } else {
-        setCustomMessage("add to start chatting!");
+        setUserMessage({
+          message: "add to start chatting!",
+          buttonText: "Add Friend",
+        });
       }
 
       // regardless if searchedUser is a friend, set info to see user profile
@@ -76,8 +106,6 @@ const Modal = () => {
         </Pressable>
       </View>
 
-      {/* <View style={tw.style("mt-5 w-full border border-t-white")} /> */}
-      {/* Todo: Display the search result of the function call */}
       <View style={tw.style("h-full items-center bg-zinc-300 pt-56")}>
         {searchedUser && (
           <View style={tw.style("items-center")}>
@@ -90,29 +118,35 @@ const Modal = () => {
               }
               style={tw.style("mb-4 h-24 w-24 rounded-full border bg-accent")}
             />
-            <Text style={tw.style("text-lg font-bold")}>
-              {searchedUser.first_name} {searchedUser.last_name}
-            </Text>
-            <Text style={tw.style("text-sm text-zinc-500")}>
-              {customMessage}
-            </Text>
-            <Pressable
-              onPress={() => {}}
-              style={({ pressed }) => [
-                tw.style("mt-4 items-center rounded-md border border-primary"),
-                pressed ? { opacity: 0.75 } : {},
-              ]}>
-              <Text
-                style={tw.style("p-2 text-base font-semibold text-primary")}>
-                Chat
+            {searchedUser.first_name && (
+              <Text style={tw.style("text-lg font-bold")}>
+                {searchedUser.first_name} {searchedUser.last_name}
               </Text>
-            </Pressable>
+            )}
+
+            <Text
+              style={tw.style("max-w-64 text-center text-sm text-zinc-500")}>
+              {userMessage.message}
+            </Text>
+            {userMessage.buttonText && (
+              <Pressable
+                onPress={() => {}}
+                style={({ pressed }) => [
+                  tw.style(
+                    "mt-4 items-center rounded-md border border-primary",
+                  ),
+                  pressed ? { opacity: 0.75 } : {},
+                ]}>
+                <Text
+                  style={tw.style("p-2 text-base font-semibold text-primary")}>
+                  {userMessage.buttonText}
+                </Text>
+              </Pressable>
+            )}
           </View>
         )}
       </View>
       {/* Todo: Clicking the user card will pop up a modal to confirm if you want to add the user as a friend */}
-
-      {/* User returned */}
     </View>
   );
 };
